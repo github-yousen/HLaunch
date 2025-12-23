f9f3b18302d51d2408c7683c390a3599ed72285d
senyou <senyou@tencent.com>
2025-12-23 16:09:19 +0800

    chore: 更新应用版本号为 0.1 并新增缓存路径节点

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "0.11"
+        versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
